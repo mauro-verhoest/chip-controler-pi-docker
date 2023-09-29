458c2432bea80ed198e552a1ef253cffa70caa23
mauro-verhoest <75494383+mauro-verhoest@users.noreply.github.com>
2023-09-29 14:33:08 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 service dbus start
+bluetoothd &
 sleep 1
 service avahi-daemon start
-cd /
-/usr/sbin/otbr-agent -I wpan0 spinel+hdlc+uart:///dev/ttyACM0?uart-baudrate=1000000 &
+#/usr/sbin/otbr-agent -I wpan0 spinel+hdlc+uart:///dev/ttyACM0?uart-baudrate=1000000 &
 sleep 1
-ot-ctl panid 0x1234
+docker exec -it otbr bash
+ot-ctl dataset init new 
+ot-ctl dataset commit active 
+#ot-ctl panid 0x1234
 ot-ctl ifconfig up
 ot-ctl thread start
+ot-ctl state 
+exit 
+sleep 2
+#./usr/bin/chip-tool-debug interactive start    
 
-sleep infinity
+/bin/bash
+#sleep infinity
